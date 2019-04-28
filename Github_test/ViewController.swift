43b10cf594ed19f11a5d4688a296e60852c7a487
masappe <masato6820@gmail.com>
2019-04-28 10:49:54 +0900

    test

--- Github_test/ViewController.swift
+++ Github_test/ViewController.swift
@@ -10,7 +10,6 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    //github test file
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.

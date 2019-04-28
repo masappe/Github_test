cdeea1c21037149cb81ca7e45173f0864ce455d7
masappe <masato6820@gmail.com>
2019-04-28 10:43:05 +0900

    Initial Commit

---
+++ Github_test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Github_test
+//
+//  Created by Masato Hayakawa on 2019/04/28.
+//  Copyright © 2019 masappe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
